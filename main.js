6a8f5252b72f16a2c01890c7e2c5fd5693e2c76a
Joshua Lavarine <joshua@lavarine.com>
2018-12-28 17:55:14 -0700

    Search body and title through searchbar

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ var swillButton = document.querySelector(".swill-btn");
 var plausibleButton = document.querySelector(".plausible-btn");
 var geniusButton = document.querySelector(".genius-btn");
 
+var searchBar = document.querySelector(".search-bar");
 
 var swillArray;
 
@@ -39,6 +40,8 @@ plausibleButton.addEventListener("click", filterByPlausible);
 
 geniusButton.addEventListener("click", filterByGenius);
 
+searchBar.addEventListener("keyup", filterInSearch);
+
 
 
 //classList THEN call deleteCard function / up / down vote functions
@@ -49,6 +52,19 @@ geniusButton.addEventListener("click", filterByGenius);
 
 reloadCards();
 
+function filterInSearch() {
+  var inputSearch = searchBar.value.toUpperCase();
+  var filteredSearch = ideaArray.filter(function (idea) {
+    var titleSearch = idea.title.toUpperCase(); 
+    var bodySearch = idea.body.toUpperCase();
+    return titleSearch.includes(inputSearch) || bodySearch.includes(inputSearch)
+  });
+  var newCard = document.querySelector("#bottom");
+  newCard.innerHTML = "";
+  filteredSearch.forEach(function(idea) {
+    addCard(idea)
+  });
+}
 
 function filterBySwill() {
   swillArray = ideaArray.filter(function(currentIdea) {
